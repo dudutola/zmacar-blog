1a2e0da65cfe16aa84a7afcfe59932063482d7e2
dulcelene machado <dudutola@gmail.com>
2024-09-06 15:42:40 +0200

    Added Article model and associated database migrations Created ArticlesController with index and show actions. Updated routes to include articles Added view template for displaying articles with title, photo, and truncated content Included placeholder image handling for articles without photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_222823) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_06_121936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_222823) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "posts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
 
-puts 'Seeding database with posts from telanon.info'
+# db/seeds.rb
 
-# call lib/tasks/cleanup.rake task
-Rake::Task['active_storage:cleanup'].invoke
+puts 'Seeding database with articles'
 
-Post.destroy_all
-puts 'Deleted all posts'
+# Create sample articles
+Article.find_or_create_by!(title: 'First Article') do |article|
+  article.content = 'This is the content of the first article.'
+  article.url = 'https://www.telanon.info/sociedade/2024/08/25/45522/adeus-da-trindade-a-professora-edith-salvaterra/'
+  article.photo.attach(io: File.open(Rails.root.join('db/fixtures/photos/example.jpg')), filename: 'example.jpg')
+end
 
-# clean up the ActiveStorage::Attachment and ActiveStorage::Blob tables
-# ActiveStorage::Attachment.destroy_all
-# ActiveStorage::Blob.destroy_all
-# ActiveStorage::Attachment.all.each { |attachment| attachment.purge }
-ActiveStorage::Attachment.all.each(&:purge)
-puts 'Deleted all ActiveStorage attachments and blobs'
+Article.find_or_create_by!(title: 'Second Article') do |article|
+  article.content = 'This is the content of the second article.'
+  article.url = 'https://www.telanon.info/suplemento/opiniao/2024/08/04/45300/suicidio-do-mlstp-partido-social-democrata-nas-mascaras-da-reforma-da-justica/'
+  article.photo.attach(io: File.open(Rails.root.join('db/fixtures/photos/example.jpg')), filename: 'example.jpg')
+end
 
-# Delete files from the storage directory
-storage_path = Rails.root.join('storage')
-# use FileUtils to remove directories only inside storage path
-# FileUtils.rm_rf(Dir.glob("#{storage_path}/*"))
-# FileUtils.rm_rf(Dir.glob("#{storage_path}/**/*"))
+Article.find_or_create_by!(title: 'Third Article') do |article|
+  article.content = 'This is the content of the third article.'
+  article.url = 'https://www.telanon.info/politica/2024/04/13/43876/sal-de-ulisses-correia-e-silva-no-tempero-da-diplomacia-do-desespero/'
+  article.photo.attach(io: File.open(Rails.root.join('db/fixtures/photos/example.jpg')), filename: 'example.jpg')
+end
 
-# Find all directories within the storage_path
-directories = Dir.glob("#{storage_path}/*").select { |entry| File.directory?(entry) }
-
-# Remove each directory
-FileUtils.rm_rf(directories)
-
-puts 'Starting to scrape telanon.info'
-ScrapeTelanonService.scrape
+puts 'Articles seeded successfully.'
